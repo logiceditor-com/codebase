6cd60750d1fcd0d748c3ebfa8691a3f416609192
Alexander Gladysh <agladysh@gmail.com>
2010-11-07 23:56:23 +0300

    error: spamming stack trace on fail()
    Otherwise it is too hard to debug where error appears.

--- pk-core/error.lua
+++ pk-core/error.lua
@@ -46,8 +46,8 @@ local error_handler_for_call = function(msg)
   if not is_error_object(msg) then
     msg = debug.traceback(msg)
     log_error(msg)
-  --else -- Useful for debugging. Keep commented out.
-  --  spam("caught: ", debug.traceback(msg[2]))
+  else
+    spam("caught: ", debug.traceback(msg[2])) -- Required for debugging
   end
   return msg
 end

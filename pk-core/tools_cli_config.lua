b3c69f114765a355d3f4d4713de1e7c78cfa9773
Alexander Gladysh <agladysh@gmail.com>
2010-12-16 07:40:59 +0300

    tools_cli_config: config may be a symlink
    Attn. VF!

--- pk-core/tools_cli_config.lua
+++ pk-core/tools_cli_config.lua
@@ -496,7 +496,7 @@ do
         for i = 1, #base_config_chunks do
           assert(do_in_environment(base_config_chunks[i], base_config))
         end 
-      elseif attr == "file" then
+      else
         local base_config_chunk = assert(loadfile(base_config_filename))
         assert(do_in_environment(base_config_chunk, base_config))
       end

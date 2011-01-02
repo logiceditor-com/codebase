f69ed0f2614e537ab7a8bcf0c29eef8afc50ae02
Alexander Gladysh <agladysh@gmail.com>
2011-01-02 16:16:29 +0300

    pk-fill-placeholders: initial commit

---
+++ src/lua/fill-placeholders/project-config/schema.lua
@@ -0,0 +1,43 @@
+--------------------------------------------------------------------------------
+-- schema.lua: fill-placeholders configuration file format
+--------------------------------------------------------------------------------
+
+local load_tools_cli_data_schema
+      = import 'pk-core/tools_cli_config.lua'
+      {
+        'load_tools_cli_data_schema'
+      }
+
+local create_config_schema
+do
+  local schema_chunk = function()
+    cfg:root
+    {
+
+--------------------------------------------------------------------------------
+
+      cfg:node "fill_placeholders"
+      {
+        cfg:existing_path "template_path";
+        cfg:path "data_path";
+        cfg:path "output_path";
+
+        -- Ruby-like capture pattern #{} is picked as a default,
+        -- since this is a command-line tool, intended to generate code.
+        -- So we have to leave traditional ${} to the generated code itself.
+        cfg:string "template_capture" { default = "#{(.-)}" };
+      };
+    }
+  end
+
+  create_config_schema = function()
+    return load_tools_cli_data_schema(
+        schema_chunk
+      )
+  end
+end
+
+return
+{
+  create_config_schema = create_config_schema;
+}

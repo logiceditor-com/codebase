9d4578dc6dc587bc8907d1661ad29fd9de495a47
BurningDaylight <dmitry.v.potapov@gmail.com>
2011-01-25 18:24:20 +0300

    src/js/object.js: implemented PK.override_object_properties(properties, rules, data)

---
+++ src/js/object.js
@@ -0,0 +1,44 @@
+PK.override_object_properties = function(properties, rules, data)
+{
+  if (!rules) return properties
+
+  if (!properties)
+  {
+    properties = {}
+  }
+
+  for (name in rules)
+  {
+    if (rules[name] === true) // just set field using data
+    {
+      if (!data[name])
+        properties[name] = data[name]
+    }
+
+    else if (typeof(rules[name]) == "function") //set and convert field using data
+    {
+      var value = rules[name](data[name])
+      if(value !== undefined)
+        properties[name] = value
+    }
+
+    else if (typeof(rules[name]) == "object") // set field to const
+    {
+      if ( rules[name].value !== undefined ) // value setter or constant
+      {
+        if (typeof(rules[name].value) == "function") // value maker
+        {
+          var value = rules[name].value(data[name])
+          if(value !== undefined)
+            properties[name] = value
+        }
+        else // const value
+        {
+          properties[name] = rules[name].value
+        }
+      }
+    }
+  }
+
+  return properties
+}

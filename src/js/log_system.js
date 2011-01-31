6478884093d1fef4fcbaebf4e420ac8aabeb26f9
BurningDaylight <dmitry.v.potapov@gmail.com>
2011-01-31 20:36:36 +0300

    src/js/log_system.js: using stack traceback in CRITICAL_ERROR

--- src/js/log_system.js
+++ src/js/log_system.js
@@ -29,7 +29,26 @@ var GUI_ERROR = function(text)
 
 var CRITICAL_ERROR = function(text)
 {
-  LOG('CRITICAL ERROR: ' + text)
+  var text_gui, text_log
+
+  if (window.printStackTrace)
+  {
+    var tb_lines = ["********** Stack Trace **********"]
+    tb_lines = tb_lines.concat(window.printStackTrace())
+    tb_lines.splice(1, 2) // Remove lines caused by call of printStackTrace()
+    tb_lines.push("**********************************")
+
+    text_log = text + "\n" + tb_lines.join("\n")
+    text_gui = text + "<br>" + tb_lines.join("<br>")
+  }
+  else
+  {
+    text_log = text + "\n" + "(Stack trace not available)" + "\n\n"
+    text_gui = text + "<br>" + "(Stack trace not available)"
+  }
+
+  LOG("\nCRITICAL ERROR: " + text_log)
+
   if (Ext.isReady)
-    Ext.Msg.alert('Failure', text)
+    Ext.Msg.alert('Failure', text_gui)
 }

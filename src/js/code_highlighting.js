fd0150c700617495e677525210e02819266f7752
BurningDaylight <dmitry.v.potapov@gmail.com>
2011-02-01 00:03:31 +0300

    src/js/code_highlighting.js: refactored since highlight.js is now optional

--- src/js/code_highlighting.js
+++ src/js/code_highlighting.js
@@ -1,10 +1,21 @@
 PK.code_highlighting = new function()
 {
+  if (!window.hljs)
+  {
+    this.highlight = function(code_text) { return '<pre>' + code_text + '</pre>' }
+    return
+  }
+
   hljs.initHighlightingOnLoad()
 
   this.highlight = function(code_text)
   {
     var viewDiv = document.getElementById("highlight-view");
+    if(!viewDiv)
+    {
+      CRITICAL_ERROR("No 'highlight-view' div necessary for code highlighting!")
+      return '<pre>' + code_text + '</pre>'
+    }
 
     viewDiv.innerHTML = '<pre><code class="lua">' + code_text + "</code></pre>"
     hljs.highlightBlock(viewDiv.firstChild.firstChild)

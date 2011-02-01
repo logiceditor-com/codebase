f2beb2935429ee219901157e83142a4ece9dea09
BurningDaylight <dmitry.v.potapov@gmail.com>
2011-02-01 22:09:02 +0300

    src/js/object.js: bugfix

--- src/js/object.js
+++ src/js/object.js
@@ -15,6 +15,10 @@ PK.override_object_properties = function(properties, rules, data)
       {
         properties[name] = data[name]
       }
+      else
+      {
+        CRITICAL_ERROR('No mandatory data for ' + name + '!')
+      }
     }
 
     else if (typeof(rules[name]) == "function") //set and convert field using data
@@ -26,16 +30,13 @@ PK.override_object_properties = function(properties, rules, data)
 
     else if (typeof(rules[name]) == "object") // set field to const
     {
-      if ( rules[name].default_value !== undefined ) // set default value if necessary
+      if (data[name] !== undefined)
       {
-        if (data[name] !== undefined)
-        {
-          properties[name] = data[name]
-        }
-        else
-        {
-          properties[name] = rules[name].default_value
-        }
+        properties[name] = data[name]
+      }
+      else if ( rules[name].default_value !== undefined ) // set default value if necessary
+      {
+        properties[name] = rules[name].default_value
       }
     }
   }

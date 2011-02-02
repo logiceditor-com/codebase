fcb0416743811d9c84666cb6a16fef9d23d95a91
BurningDaylight <dmitry.v.potapov@gmail.com>
2011-02-02 16:40:55 +0300

    src/js/clone.js: using faster version

--- src/js/clone.js
+++ src/js/clone.js
@@ -1,5 +1,17 @@
+// PK.clone = function(obj)
+// {
+//   // Note: Don't use Ext.encode() since it removes holes ('undefined') from arrays!
+//   return Ext.decode(JSON.stringify(obj))
+// }
+
+// TODO: Critical thing, write tests!
 PK.clone = function(obj)
 {
-  // Note: Don't use Ext.encode() since it removes holes ('undefined') from arrays!
-  return Ext.decode(JSON.stringify(obj))
+  if (typeof(obj) != "object" || obj == null)
+    return obj
+
+  var clone = obj.constructor()
+  for(var i in obj)
+    clone[i] = PK.clone(obj[i])
+  return clone
 }

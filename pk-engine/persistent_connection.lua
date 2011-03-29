b0a7d748dfe86ca7b79af7f6c05c367e755d4ed2
Alexander Gladysh <agladysh@gmail.com>
2011-03-30 01:44:27 +0400

    persistent_connection: fixed line length; minor comment wording tuning

--- pk-engine/persistent_connection.lua
+++ pk-engine/persistent_connection.lua
@@ -28,17 +28,20 @@ local log, dbg, spam, log_error = make_loggers("persistent_connection", "PCO")
 
 -- Creates persistent connection object, with interface as follows:
 --
---   send() - Proxies 'send' call to socket, detecting whether socket was closed.
+--   send() - Proxies 'send' call to socket,
+--            detecting whether socket was closed.
 --            If socket was closed next time we use the object,
 --            new connection will be created.
 --            Error is returned to user as is in any way.
 --
---   receive() - Proxies 'send' call to socket, detecting whether socket was closed
+--   receive() - Proxies 'send' call to socket,
+--            detecting whether socket was closed
 --            If socket was closed next time we use the object,
 --            new connection will be created.
 --            Error is returned to user as is in any way.
 --
---   close() - Explicitly closes current connection so next time we use the object,
+--   close() - Explicitly closes current connection,
+--             so next time we use the object,
 --             new connection will be created.
 --
 -- All other functions are proxied directly to socket.
@@ -153,7 +156,9 @@ do
 
   local delegated_method = function(name)
     return function(self, ...)
-      return assert(self.conn_proxy_, "not connected")[name](self.conn_proxy_, ...)
+      return assert(
+          self.conn_proxy_, "not connected"
+        )[name](self.conn_proxy_, ...)
     end
   end
 
@@ -171,8 +176,11 @@ do
       -- Undocumented methods for select()
       getfd = delegated_method("getfd");
       dirty = delegated_method("dirty");
+
       -- Extra API
-      refresh = refresh; -- Note: can't name connect(), socket already has this function.
+
+      -- Note: can't name connect(), socket already has such function.
+      refresh = refresh;
       --
       connector_ = connector;
       conn_proxy_ = nil;

397ac41d89901e37dba8e70e391b3ede99a97bef
Burning Daylight <dmitry.v.potapov@gmail.com>
2011-04-12 22:46:59 +0400

    src/js/string.js: removed duplicate function

--- src/js/string.js
+++ src/js/string.js
@@ -71,55 +71,6 @@ PK.split_using_placeholders = function(s, keys)
     for (var i = 0; i < keys.length; i++)
     {
       var placeholder = '${' + keys[i] + '}'
-      if( s.indexOf(placeholder) < 0 )
-        break
-      s = s.replace(placeholder, SEPARATOR + placeholder + SEPARATOR)
-    }
-  }
-
-  var splitted = s.split(SEPARATOR)
-
-  var result = []
-  for(var i = 0; i < splitted.length; i++)
-  {
-    if (splitted[i].match(/\$\{\d+\}/g))
-    {
-      result.push(Number(splitted[i].replace(/[\$\{\}]/g, "")))
-    }
-    else if (splitted[i] != "")
-    {
-      result.push(splitted[i])
-    }
-
-  }
-
-  return result
-}
-
-// TODO: Note:
-//   1. 's' must contain no separators!
-//   2. Very non-optimal, reduce to one iteration
-PK.split_using_placeholders = function(s, keys)
-{
-  var SEPARATOR = '{%_SEP_%}'
-
-  var num_placehoders = 0, placeholder_found = true
-
-  while (true)
-  {
-    num_placehoders++
-    var placeholder = '${' + num_placehoders + '}'
-    if( s.indexOf(placeholder) < 0 )
-      break
-    s = s.replace(placeholder, SEPARATOR + placeholder + SEPARATOR)
-  }
-  num_placehoders--
-
-  if(keys)
-  {
-    for (var i = 0; i < keys.length; i++)
-    {
-      var placeholder = '${' + keys[i] + '}'
       if( s.indexOf(placeholder) >= 0 )
       {
         s = s.replace(placeholder, SEPARATOR + placeholder + SEPARATOR)

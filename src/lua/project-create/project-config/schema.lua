ed7d7800c253bc885d4d737aff680f8775c3b59d
Vladimir Fedin <vl.fedin@gmail.com>
2011-05-10 18:38:49 +0400

    Project-create added

---
+++ src/lua/project-create/project-config/schema.lua
@@ -0,0 +1,38 @@
+--------------------------------------------------------------------------------
+-- schema.lua: project-create configuration file format
+--------------------------------------------------------------------------------
+
+local load_tools_cli_data_schema
+      = import 'pk-core/tools_cli_config.lua'
+      {
+        'load_tools_cli_data_schema'
+      }
+
+local create_config_schema
+do
+  local schema_chunk = function()
+    cfg:root
+    {
+
+--------------------------------------------------------------------------------
+
+      cfg:node "project_create"
+      {
+        cfg:existing_path "metamanifest_path";
+        cfg:path "root_project_path";
+        cfg:boolean "debug" { default = false };
+      };
+    }
+  end
+
+  create_config_schema = function()
+    return load_tools_cli_data_schema(
+        schema_chunk
+      )
+  end
+end
+
+return
+{
+  create_config_schema = create_config_schema;
+}

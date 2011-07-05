64269cb69fb1fce249d310956692addddcee1856
Burning Daylight <dmitry.v.potapov@gmail.com>
2011-07-06 03:45:06 +0400

    www/js/filters.js: using form field editors

--- www/js/filters.js
+++ www/js/filters.js
@@ -21,27 +21,27 @@ PKAdmin.filters = new function()
   }
 
 
-  this.make_string_filter = function(field_name, field_title)
+  this.make_common_filter = function(value_type, field_name, field_title, params)
   {
     var render_comparision_params = function()
     {
-      return {
-        xtype: 'textfield'
-      }
+      return PK.common_custom_editors.make_form_field_editor(
+        value_type, params
+      )
     }
 
     var render_interval_params = function()
     {
       return [
-        {
-          xtype: 'textfield'
-        },
+        PK.common_custom_editors.make_form_field_editor(
+          value_type, params
+        ),
         { xtype: 'spacer', width: 10 },
         { xtype: 'label', html: "&dash;" },
         { xtype: 'spacer', width: 10 },
-        {
-          xtype: 'textfield'
-        }
+        PK.common_custom_editors.make_form_field_editor(
+          value_type, params
+        ),
       ]
     }
 
@@ -94,60 +94,26 @@ PKAdmin.filters = new function()
     }
   }
 
-  // TODO: Implement filters
-  this.make_int_filter = this.make_string_filter
-  this.make_enum_filter = this.make_string_filter
-  this.make_bool_filter = this.make_string_filter
-  this.make_date_filter = this.make_string_filter
-  this.make_db_ids_filter = this.make_string_filter
-  this.make_money_filter = this.make_string_filter
-
 
   this.make_filter = function(value_type, field_name, field_title, params)
   {
     switch (Number(value_type))
     {
       case PK.table_element_types.STRING:
-        return this.make_string_filter(field_name, field_title)
-        break
-
       case PK.table_element_types.INT:
-        return this.make_int_filter(field_name, field_title, params ? params.precision : undefined)
-        break
-
       case PK.table_element_types.ENUM:
-        if(!params.enum_items)
-        {
-          CRITICAL_ERROR("Enum_items not defined for " + field_title)
-          return undefined
-        }
-        return this.make_enum_filter(field_name, field_title, params.enum_items)
-        break;
-
       case PK.table_element_types.BOOL:
-        return this.make_bool_filter(field_name, field_title)
-        break
-
       case PK.table_element_types.DATE:
-        return this.make_date_filter(field_name, field_title, params.print_time)
-        break
-
       case PK.table_element_types.PHONE:
       case PK.table_element_types.MAIL:
-        return this.make_string_filter(field_name, field_title)
-        break
-
       case PK.table_element_types.DB_IDS:
-        return this.make_db_ids_filter(field_name, field_title)
+      case PK.table_element_types.MONEY:
+        return this.make_common_filter(value_type, field_name, field_title, params)
         break
 
       case PK.table_element_types.BINARY_DATA:
         return undefined // No filter for binary data
         break
-
-      case PK.table_element_types.MONEY:
-        return this.make_money_filter(field_name, field_title);
-        break;
 
       default:
         return undefined

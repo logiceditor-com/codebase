7b124169fdc5bfc5e2e93cff8d2bbfcb4041a07f
Burning Daylight <dmitry.v.potapov@gmail.com>
2011-07-11 21:11:49 +0400

    www/js/client_settings.js: calculation of default column width

--- www/js/client_settings.js
+++ www/js/client_settings.js
@@ -3,11 +3,10 @@ PKAdmin.client_settings = new function()
   var DEFAULT_TABLE_COLUMN_SETTINGS =
   {
     hidden  : false,
-    width   : 250
+    width   : 100
   }
 
   var table_column_settings_ = {}
-
 
   this.table_column = function(table_name, column_name)
   {
@@ -18,8 +17,12 @@ PKAdmin.client_settings = new function()
     {
       table_column_settings_[table_name][column_name] = PK.clone(DEFAULT_TABLE_COLUMN_SETTINGS)
 
-      //table_column_settings_[table_name][column_name].width =
-      //  Ext.util.TextMetrics.measure(default_column, I18N(column_name).width)
+      table_column_settings_[table_name][column_name].width =
+       Ext.util.TextMetrics.measure(
+           Ext.get('navigator-menu-data'),
+           I18N(column_name)
+         ).width
+         + 25
     }
 
     return table_column_settings_[table_name][column_name]

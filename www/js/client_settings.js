36d9575429e1622e4b2d55020e6f4730e41a9fe5
Burning Daylight <dmitry.v.potapov@gmail.com>
2011-07-11 19:02:57 +0400

    www/js/client_settings.js: dummy store for client settings

---
+++ www/js/client_settings.js
@@ -0,0 +1,22 @@
+PKAdmin.client_settings = new function()
+{
+  var DEFAULT_TABLE_COLUMN_SETTINGS =
+  {
+    hidden  : false,
+    width   : 250
+  }
+
+  var table_column_settings_ = {}
+
+
+  this.table_column = function(table_name, column_name)
+  {
+    if (!table_column_settings_[table_name])
+      table_column_settings_[table_name] = {}
+
+    if (!table_column_settings_[table_name][column_name])
+      table_column_settings_[table_name][column_name] = PK.clone(DEFAULT_TABLE_COLUMN_SETTINGS)
+
+    return table_column_settings_[table_name][column_name]
+  }
+}

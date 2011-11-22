a774a45d1804b7805b9217dd7a52a5e78684ad55
Valeriy Skurikhin <Keen.vs@gmail.com>
2011-11-22 13:45:48 +0400

    Added config schema for the tool

---
+++ src/lua/update-subtrees/project-config/schema.lua
@@ -0,0 +1,41 @@
+local load_tools_cli_data_schema
+      = import 'pk-core/tools_cli_config.lua'
+      {
+        'load_tools_cli_data_schema'
+      }
+
+--------------------------------------------------------------------------------
+
+local create_config_schema
+do
+  local schema_chunk = function()
+    cfg:root
+    {
+      cfg:node "update_config"
+      {
+        cfg:variant "action"
+        {
+          variants =
+          {
+            ["update"] = {
+              cfg:existing_path "manifest_path";
+              cfg:optional_string "subtree_name";
+              cfg:optional_string "branch_name";
+            };
+          };
+        };
+      };
+    }
+  end
+
+  create_config_schema = function()
+    return load_tools_cli_data_schema(schema_chunk)
+  end
+end
+
+--------------------------------------------------------------------------------
+
+return
+{
+  create_config_schema = create_config_schema;
+}

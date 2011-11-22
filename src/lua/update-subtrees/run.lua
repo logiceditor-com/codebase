bfe31cacf75e199b27adef9262a17ee51df9ae10
Valeriy Skurikhin <Keen.vs@gmail.com>
2011-11-22 13:50:42 +0400

    Addeed support for --feature-branch option

--- src/lua/update-subtrees/run.lua
+++ src/lua/update-subtrees/run.lua
@@ -34,7 +34,8 @@ local git_load_config,
       git_is_dirty,
       git_has_untracked_files,
       git_get_tracking_branch_name_of_HEAD,
-      git_are_branches_different
+      git_are_branches_different,
+      git_read
       = import 'lua-aplicado/shell/git.lua'
       {
         'git_load_config',
@@ -46,7 +47,8 @@ local git_load_config,
         'git_is_dirty',
         'git_has_untracked_files',
         'git_get_tracking_branch_name_of_HEAD',
-        'git_are_branches_different'
+        'git_are_branches_different',
+        'git_read'
       }
 
 local load_project_manifest
@@ -55,34 +57,44 @@ local load_project_manifest
         'load_project_manifest'
       }
 
---------------------------------------------------------------------------------
+local load_tools_cli_config,
+      freeform_table_value
+      = import 'pk-core/tools_cli_config.lua'
+      {
+        'load_tools_cli_config',
+        'freeform_table_value'
+      }
 
-local PROJECT_PATH
+local create_config_schema
+      = import 'update-subtrees/project-config/schema.lua'
+      {
+        'create_config_schema',
+      }
 
 --------------------------------------------------------------------------------
 
 local ACTIONS = { }
 
+local SCHEMA = create_config_schema()
+
+local CONFIG, ARGS
+
+local EXTRA_HELP 
 --------------------------------------------------------------------------------
 
-ACTIONS.update = function(manifest_path, subtree_name, cluster_name)
-  cluster_name = cluster_name or "UNKNOWN_CLUSTER" -- irrelevant to this tool
-
-  arguments(
-      "string", manifest_path,
-      "string", cluster_name
-    )
-  optional_arguments(
-      "string", subtree_name
-    )
+ACTIONS.update = function()
+  local param = freeform_table_value(CONFIG.update_config.action.param)  
+  local manifest_path = param.manifest_path
+  local subtree_name = param.subtree_name
+  local branch_name = param.branch_name
 
   local manifest = load_project_manifest(
         manifest_path,
-        PROJECT_PATH,
-        cluster_name
+        CONFIG.PROJECT_PATH,
+        "UNKNOWN_CLUSTER" -- irrelevant to this tool
       )
 
-  local subtrees = assert(manifest.subtrees)
+  local subtrees = assert(manifest.subtrees, "manifest subtrees are missing")
 
   local git_configs = setmetatable(
       { },
@@ -119,10 +131,25 @@ ACTIONS.update = function(manifest_path, subtree_name, cluster_name)
   for i = 1, #subtrees do
     local subtree = subtrees[i]
 
-    local git_dir = assert(subtree.git)
-    local git_remote_name = assert(subtree.name)
-    local git_remote_url = assert(subtree.url)
-    local subtree_path = assert(subtree.path)
+    local git_dir = assert(subtree.git, "project path is not set")
+    local git_remote_name = assert(subtree.name, "subtree name is not set")
+    local git_remote_url = assert(subtree.url, "subtree remote url is not set")
+    local subtree_path = assert(subtree.path, "subtree path is not set")
+    local branch_to_update
+
+    if branch_name then
+      local reply = git_read(
+          git_dir, 'ls-remote', "--heads", git_remote_url, branch_name
+        )
+      local remote_branch_exists = reply and reply ~= ""
+      if remote_branch_exists then
+        branch_to_update = branch_name
+      else
+        branch_to_update = assert(subtree.branch, "subtree branch is not set")
+      end
+    else
+      branch_to_update = assert(subtree.branch, "subtree branch is not set")
+    end
 
     if subtree_name and git_remote_name ~= subtree_name then
       log("skipping", git_remote_name)
@@ -144,7 +171,7 @@ ACTIONS.update = function(manifest_path, subtree_name, cluster_name)
         end
 
         if lfs.attributes(git_dir.."/"..subtree_path) then
-          log("initializing remote", git_remote_name, "not initializing subtree in", subtree_path)
+          log("initializing remote", git_remote_name, "not initializing subtree in" ,subtree_path)
           git_remote_add(git_dir, git_remote_name, git_remote_url, true) -- With fetch
         else
           log("initializing subtree", git_remote_name, "in", subtree_path)
@@ -153,7 +180,7 @@ ACTIONS.update = function(manifest_path, subtree_name, cluster_name)
               git_dir,
               git_remote_name,
               git_remote_url,
-              assert(subtree.branch),
+              branch_to_update,
               subtree_path,
               "merged " .. git_remote_name .. " as a subtree to " .. subtree_path,
               false -- Not interactive
@@ -165,7 +192,7 @@ ACTIONS.update = function(manifest_path, subtree_name, cluster_name)
         git_pull_subtree(
             git_dir,
             git_remote_name,
-            assert(subtree.branch)
+            branch_to_update
           )
       end
     end
@@ -177,14 +204,37 @@ end
 --------------------------------------------------------------------------------
 
 local run = function(...)
-  -- TODO: WTF is with argument order?
-  PROJECT_PATH = assert(select(1, ...), "missing project root")
-  local manifest_path = assert(select(2, ...), "missing manifest path")
-  local action_name = assert(select(3, ...), "missing action name")
+  CONFIG, ARGS = assert(load_tools_cli_config(
+      function(args)
+        return
+        {
+          -- Reading args by indices was left for compatibility
+          -- with update-subtrees scripts.
+          PROJECT_PATH = args["--root"] or args[1];
+          ["update_config"] =
+          {
+            action =
+            {
+              name = args["--action"] or args[3];
+              param =
+              {
+                manifest_path = args["--manifest-path"] or args[2];
+                subtree_name = args["--subtree-name"] or args[4];
+                branch_name = args["--feature-branch"];
+              };
+            };
+          }
+        }
+      end,
+      EXTRA_HELP,
+      SCHEMA,
+      nil,
+      nil,
+      ...
+    ))
 
-  assert(ACTIONS[action_name], "unknown action")(manifest_path, select(4, ...))
+  assert(ACTIONS[CONFIG.update_config.action.name], "unknown action")()
 end
-
 --------------------------------------------------------------------------------
 
 return

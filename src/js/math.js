ca91b6303873298c12e8d1c66ac98e2f03808eb1
Mikhail Medvedev <moorcock84@gmail.com>
2012-02-01 01:05:39 +0400

    math.js: jsdoc, code style

--- src/js/math.js
+++ src/js/math.js
@@ -1,15 +1,36 @@
+//------------------------------------------------------------------------------
+// Math functions
+//------------------------------------------------------------------------------
+
 PK.check_namespace('Math');
 
+/**
+ * Returns random int number from m to n.
+ *
+ * @param m
+ * @param n
+ */
 PK.Math.random_int = function(m, n)
 {
-  return Math.floor( Math.random() * (n - m + 1) ) + m;
+  return Math.floor(Math.random() * (n - m + 1)) + m;
 }
 
+/**
+ * If number <= a returns a; If v >= b returns b; otherwise returns v.
+ *
+ * @param v
+ * @param a
+ * @param b
+ */
 PK.Math.clamp = function(v, a, b)
 {
-  if (a == b )
-    return a
+  if (a == b ) return a;
 
-  if (b < a) { var tmp = a; a=b; b=tmp; }
-  return Math.min( Math.max(v,a), b )
+  if (b < a)
+  {
+    var tmp = a;
+    a = b;
+    b = tmp;
+  }
+  return Math.min(Math.max(v, a), b);
 }

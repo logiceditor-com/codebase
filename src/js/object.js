c1176a6efab1d6a44e86ebc52a0faaf7db98ad14
Mikhail Medvedev <moorcock84@gmail.com>
2012-02-01 01:06:04 +0400

    object.js: jsdoc, code style

--- src/js/object.js
+++ src/js/object.js
@@ -1,39 +1,56 @@
-// Note: This method has a little use since we have JSON.stringify()
+//------------------------------------------------------------------------------
+// Utilities working with objects
+//------------------------------------------------------------------------------
+
+/**
+ * Serialize object.
+ * Note: This method has a little use since we have JSON.stringify()
+ *
+ * @param object
+ * @param br
+ */
 PK.serialize_object = function(object, br)
 {
-  if (br === undefined)
-    br = "\n"
+  if (br === undefined) br = "\n";
 
   if (typeof object == "string")
-    return "'" + object + "'"
+  {
+    return "'" + object + "'";
+  }
 
-  var text = ""
+  var text = "";
 
   for (var prop in object)
   {
-    var value = object[prop]
-    var type = typeof value
+    var value = object[prop];
+    var type = typeof value;
     switch (type)
     {
       case "object":
-        text += prop + ": "+ "[Object]" + br
-        break
+        text += prop + ": "+ "[Object]" + br;
+        break;
       default:
-        text += prop + ": "+ value + br
+        text += prop + ": "+ value + br;
     }
   }
 
-  return text
+  return text;
 }
 
-
+/**
+ * Replace object properties following the rules, return object.
+ *
+ * @param properties
+ * @param rules
+ * @param data
+ */
 PK.override_object_properties = function(properties, rules, data)
 {
-  if (!rules) return properties
+  if (!rules) return properties;
 
   if (!properties)
   {
-    properties = {}
+    properties = {};
   }
 
   for (var name in rules)
@@ -42,37 +59,44 @@ PK.override_object_properties = function(properties, rules, data)
     {
       if (data[name] !== undefined)
       {
-        properties[name] = data[name]
+        properties[name] = data[name];
       }
       else
       {
-        CRITICAL_ERROR('No mandatory data for ' + name + '!')
+        CRITICAL_ERROR('No mandatory data for ' + name + '!');
       }
     }
 
     else if (typeof(rules[name]) == "function") //set and convert field using data
     {
-      var value = rules[name](data[name])
+      var value = rules[name](data[name]);
       if(value !== undefined)
-        properties[name] = value
+      {
+        properties[name] = value;
+      }
     }
 
     else if (typeof(rules[name]) == "object") // set field to const
     {
       if (data[name] !== undefined)
       {
-        properties[name] = data[name]
+        properties[name] = data[name];
       }
-      else if ( rules[name].default_value !== undefined ) // set default value if necessary
+      else if (rules[name].default_value !== undefined ) // set default value if necessary
       {
-        properties[name] = rules[name].default_value
+        properties[name] = rules[name].default_value;
       }
     }
   }
 
-  return properties
+  return properties;
 }
 
+/**
+ * Count object properties.
+ *
+ * @param obj
+ */
 PK.count_properties = function(obj)
 {
   var length = 0;
@@ -91,14 +115,17 @@ PK.count_properties = function(obj)
   return length;
 }
 
-/*
-  { name1: 1, name2: 2 } => { 1: name1, 2: name2 }
-*/
-
-PK.Swap_keys_and_values = function(value_by_name)
+/**
+ * { name1: 1, name2: 2 } => { 1: name1, 2: name2 }
+ *
+ * @param value_by_name
+ */
+PK.swap_keys_and_values = function(value_by_name)
 {
-  var name_by_value = {}
+  var name_by_value = {};
   for (var name in value_by_name)
-    name_by_value[value_by_name[name]] = name
-  return name_by_value
+  {
+    name_by_value[value_by_name[name]] = name;
+  }
+  return name_by_value;
 }

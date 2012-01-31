9e6235d0d5e862e97bbc2a1f643ec73093413891
Mikhail Medvedev <moorcock84@gmail.com>
2012-02-01 01:03:31 +0400

    Added functions to math.js from pk-hb/client

--- src/js/math.js
+++ src/js/math.js
@@ -4,3 +4,12 @@ PK.Math.random_int = function(m, n)
 {
   return Math.floor( Math.random() * (n - m + 1) ) + m;
 }
+
+PK.Math.clamp = function(v, a, b)
+{
+  if (a == b )
+    return a
+
+  if (b < a) { var tmp = a; a=b; b=tmp; }
+  return Math.min( Math.max(v,a), b )
+}
